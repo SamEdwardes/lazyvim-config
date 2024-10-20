93f5eabcf10fcc685b111cf417933f692f03c784
SamEdwardes <edwardes.s@gmail.com>
2024-10-20 08:52:46 -0700

    Added my own initial changes to LazyVim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Set some VS Code only keymaps
+if vim.g.vscode then
+  -- VSCode extension
+  local vscode = require("vscode")
+
+  -- Open VSCode file explorer
+  vim.keymap.set({ "n" }, "<leader>e", function()
+    vscode.with_insert(function()
+      vscode.action("workbench.view.explorer")
+    end)
+  end)
+end
