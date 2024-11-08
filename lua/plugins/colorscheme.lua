9763da0fab239c40324684e6c8b0e275c38e5aa5
SamEdwardes <edwardes.s@gmail.com>
2024-11-08 13:32:44 -0800

    Using catppuccin theme and colorful-winsep.nvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    opts = {
+      integrations = {
+        colorful_winsep = {
+          enabled = true,
+          color = "red",
+        },
+      },
+    },
+  },
+}
