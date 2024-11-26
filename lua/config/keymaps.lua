d707f8f571596a25304bec962684aa1f03873f82
SamEdwardes <edwardes.s@gmail.com>
2024-11-26 13:39:11 -0800

    General updates
    - Tab keymaps
    - overseer updates
    - just file support
    - djhtml formating

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- tabs
+vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+vim.keymap.set("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
+vim.keymap.set("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnew<cr>", { desc = "New Tab" })
+vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnext<cr>", { desc = "New Tab" })
+vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
 -- Set some VS Code only keymaps
 if vim.g.vscode then
   -- VSCode extension
@@ -25,6 +35,15 @@ if vim.g.vscode then
   -- Find in files
   vim.keymap.set({ "n" }, "<leader>sg", function()
     vscode.action("workbench.action.findInFiles")
+  end)
+
+  -- Code actions ================================================================
+  vim.keymap.set({ "n" }, "<leader>cs", function()
+    vscode.action("outline.toggleVisibility")
+  end)
+
+  vim.keymap.set({ "n" }, "<leader>cr", function()
+    vscode.action("editor.action.rename")
   end)
 
   -- File pane ================================================================
--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,12 +1,12 @@
 return {
   "stevearc/overseer.nvim",
   keys = {
-    { "<leader>tw", "<cmd>OverseerToggle<cr>", desc = "Task list" },
-    { "<leader>to", "<cmd>OverseerRun<cr>", desc = "Run task" },
+    { "<leader>tl", "<cmd>OverseerToggle<cr>", desc = "Task list" },
+    { "<leader>tr", "<cmd>OverseerRun<cr>", desc = "Run task" },
     { "<leader>tq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
     { "<leader>ti", "<cmd>OverseerInfo<cr>", desc = "Overseer Info" },
     { "<leader>tb", "<cmd>OverseerBuild<cr>", desc = "Task builder" },
-    { "<leader>tt", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
+    { "<leader>ta", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
     { "<leader>tc", "<cmd>OverseerClearCache<cr>", desc = "Clear cache" },
   },
 }
