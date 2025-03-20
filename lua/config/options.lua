b6efe515ac723d82b660ded273fdc3f8bbe06fa1
SamEdwardes <edwardes.s@gmail.com>
2025-03-20 13:44:15 -0700

    Update LazyVim and and remote-nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,13 @@
 -- Disable concealing by default
 -- https://github.com/LazyVim/LazyVim/discussions/269
 vim.wo.conceallevel = 0
+
+-- Custom logic for defining the root dir. Cwd is the directory
+-- you open neo vim in. Root dir is determined by this logic.
+-- https://github.com/LazyVim/LazyVim/discussions/5519
+vim.g.root_spec = { { ".git", "lua" }, "lsp", "cwd" }
+
+-- Custom options when in VS Code
+if vim.g.vscode then
+  print("I am in VS Code!")
+end
