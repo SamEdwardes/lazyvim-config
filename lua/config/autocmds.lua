d2d3163fd797971e7c5c0be64288d72d910a50d7
SamEdwardes <edwardes.s@gmail.com>
2025-03-20 13:43:43 -0700

    Add an auto command that disables spelling in VS Code

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable spell check on markdown by default
+-- https://github.com/LazyVim/LazyVim/discussions/392
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    if vim.g.vscode then
+      vim.opt_local.spell = false
+    else
+      vim.opt_local.spell = true
+    end
+  end,
+})
