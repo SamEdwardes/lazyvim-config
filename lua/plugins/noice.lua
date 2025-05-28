735b27119fe0eb30b46bdaa91b937bd74dc337e6
SamEdwardes <edwardes.s@gmail.com>
2025-05-27 17:19:13 -0700

    Improve how noice shows long messages

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,6 +3,25 @@ return {
     "folke/noice.nvim",
     enabled = true,
     opts = {
+      presets = {
+        long_message_to_split = true, -- long messages will be sent to a split
+      },
+      routes = {
+        -- {
+        --   -- Send long message outputs to split view. In notify
+        --   -- they are hard to read because they get cut off.
+        --   filter = {
+        --     event = "msg_show",
+        --     any = {
+        --       { min_length = 80 },
+        --     },
+        --   },
+        --   view = "split",
+        -- },
+      },
+      messages = {
+        view = "notify",
+      },
       cmdline = {
         format = {
           cmdline = { pattern = "^:", icon = ":", lang = "vim" },
