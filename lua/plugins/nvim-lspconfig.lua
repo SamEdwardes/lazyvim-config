249c33927fa571140737fae4efd334a8f9496e8f
SamEdwardes <edwardes.s@gmail.com>
2025-05-30 07:57:54 -0700

    Get djlsp working

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,23 +2,10 @@ return {
   "neovim/nvim-lspconfig",
   ---@class PluginLspOpts
   opts = {
-    ---@type lspconfig.options
     inlay_hints = { enabled = false },
     servers = {
       -- Django Template LSP: https://github.com/fourdigits/django-template-lsp
       djlsp = {},
-    },
-    -- you can do any additional lsp server setup here
-    -- return true if you don't want this server to be setup with lspconfig
-    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-    setup = {
-      djslp = function(_, opts)
-        require("lspconfig").djlsp.setup({
-          cmd = { "djlsp" },
-        })
-      end,
-      -- Specify * to use this function as a fallback for any server
-      -- ["*"] = function(server, opts) end,
     },
   },
 }
