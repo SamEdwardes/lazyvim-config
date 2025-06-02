8b3b329fc1b5ab8553ea36f5b195407ee6c970c5
SamEdwardes <edwardes.s@gmail.com>
2025-06-02 08:11:52 -0700

    Use blink for cmdline

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,9 +1,21 @@
 return {
   "saghen/blink.cmp",
-  ---@module 'blink.cmp'
   opts = {
     cmdline = {
-      enabled = false,
+      enabled = true,
+      completion = {
+        menu = { auto_show = true },
+        list = {
+          selection = { preselect = false },
+        },
+      },
+      keymap = {
+        preset = "enter",
+        ["<C-y>"] = { "show_and_insert" },
+        ["<CR>"] = { "accept_and_enter", "fallback" },
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
     },
     completion = {
       menu = {
