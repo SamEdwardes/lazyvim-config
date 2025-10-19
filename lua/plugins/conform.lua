67855b2c6128bf538664864f210431164e745437
SamEdwardes <edwardes.s@gmail.com>
2025-10-19 14:45:29 -0700

    Get markdown formating and linting work as epxected

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,8 +12,11 @@ return {
       astro = {
         "prettierd",
       },
+      css = {
+        "prettierd",
+      },
       ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-      ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" }
+      ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
     },
     formatters = {
       djhtml = {
@@ -22,22 +25,22 @@ return {
         stdin = false,
       },
       ["markdown-toc"] = {
-      condition = function(_, ctx)
-        for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-          if line:find("<!%-%- toc %-%->") then
-            return true
+        condition = function(_, ctx)
+          for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+            if line:find("<!%-%- toc %-%->") then
+              return true
+            end
           end
-        end
-      end,
-    },
-    ["markdownlint-cli2"] = {
-      condition = function(_, ctx)
-        local diag = vim.tbl_filter(function(d)
-          return d.source == "markdownlint"
-        end, vim.diagnostic.get(ctx.buf))
-        return #diag > 0
-      end,
-    },
+        end,
+      },
+      ["markdownlint-cli2"] = {
+        condition = function(_, ctx)
+          local diag = vim.tbl_filter(function(d)
+            return d.source == "markdownlint"
+          end, vim.diagnostic.get(ctx.buf))
+          return #diag > 0
+        end,
+      },
     },
   },
 }
