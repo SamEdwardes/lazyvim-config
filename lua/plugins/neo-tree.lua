4979cb145dcf0ba33117c1c39199b6c582d1619d
SamEdwardes <edwardes.s@gmail.com>
2025-10-23 10:58:45 -0700

    Update neo-tree so that leader, leader opens in current working directory instead of root directory.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   cmd = "Neotree",
   keys = {
-    { "<leader>ee", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
-    { "<leader>eE", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    { "<leader>ee", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    { "<leader>eE", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
   },
 }
