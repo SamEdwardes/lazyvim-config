60a5e64649029f068f3346104a230b0faf7d0b5c
SamEdwardes <edwardes.s@gmail.com>
2025-12-18 12:05:08 -0800

    Better mini brackets

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -17,11 +17,11 @@ return {
     opts = {
       modes = { insert = true, command = true, terminal = true },
       -- skip autopair when next character is one of these
-      -- skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-      skip_next = false,
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip_next = false,
       -- skip autopair when the cursor is inside these treesitter nodes
-      -- skip_ts = { "string" },
-      skip_ts = false,
+      skip_ts = { "string" },
+      -- skip_ts = false,
       -- skip autopair when next character is closing pair
       -- and there are more closing pairs than opening pairs
       skip_unbalanced = true,
